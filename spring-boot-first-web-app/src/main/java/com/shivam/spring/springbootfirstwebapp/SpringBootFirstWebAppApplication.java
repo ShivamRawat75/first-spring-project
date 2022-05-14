2e0cc4d0a05ec3defa10cfbfe856d02854eeef98
Shivam Rawat <ajsivamaj@gmail.com>
2022-05-14 10:24:01 +0530

    first commit

---
+++ spring-boot-first-web-app/src/main/java/com/shivam/spring/springbootfirstwebapp/SpringBootFirstWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.shivam.spring.springbootfirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstWebAppApplication.class, args);
+	}
+
+}
---
+++ spring-boot-first-web-app/src/test/java/com/shivam/spring/springbootfirstwebapp/SpringBootFirstWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.spring.springbootfirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
